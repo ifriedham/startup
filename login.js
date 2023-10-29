210b795ba46da0d6cd9b0eeeb1ddb19f7415419c
ifriedham <noah10101@live.com>
2023-10-28 21:35:02 -0600

    login button borked, but fixing

--- login.js
+++ login.js
@@ -1,27 +1,66 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const overlay = document.getElementById("overlay");
-  const loginCard = document.getElementById("loginCard");
-  const showCardButton = document.getElementById("showCard");
-  const closeCardButton = document.getElementById("closeCard");
-
-  // check if user is logged in
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
-
-  if (!isLoggedIn) {
-    // Show the overlay and login card by default
-    overlay.style.display = "block";
-    loginCard.style.display = "block";
-  }
-
-  showCardButton.addEventListener("click", function () {
-    overlay.style.display = "block";
-    loginCard.style.display = "block";
-  });
-
+document.addEventListener('DOMContentLoaded', function () {
+    const overlay = document.getElementById('overlay');
+    const loginCard = document.getElementById('loginCard');
+    const loginButton = document.getElementById('login');
+    const headerButton = document.getElementById('headerButton');
+    const usernameInput = document.getElementById('usernameInput');
+    const usernameDisplay = document.getElementById('usernameDisplay');
+    const logoutPopup = document.getElementById('logoutPopup');
+    const closeLogoutPopupButton = document.getElementById('closeLogoutPopup');
   
-
-  closeCardButton.addEventListener("click", function () {
-    overlay.style.display = "none";
-    loginCard.style.display = "none";
+    // Check if user is logged in
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+  
+    // Function to open the login popup
+    function openLoginPopup() {
+      overlay.style.display = 'block';
+      loginCard.style.display = 'block';
+    }
+  
+    // Show login popup by default if not logged in
+    if (!isLoggedIn) {
+      openLoginPopup();
+    } else {
+      headerButton.textContent = 'Logout';
+      usernameDisplay.textContent = localStorage.getItem('username');
+    }
+  
+    // Handle form submission and closing the login popup
+    loginButton.addEventListener('click', function () {
+      const username = usernameInput.value;
+  
+      if (username) {
+        headerButton.textContent = 'Logout';
+        usernameDisplay.textContent = username;
+  
+        localStorage.setItem('isLoggedIn', 'true');
+        localStorage.setItem('username', username);
+  
+        overlay.style.display = 'none';
+        loginCard.style.display = 'none';
+      }
+    });
+  
+    // Handle logout and show "Logout Successful" popup
+    headerButton.addEventListener('click', function () {
+      if (isLoggedIn) {
+        localStorage.removeItem('isLoggedIn');
+        localStorage.removeItem('username');
+        headerButton.textContent = 'Login';
+        usernameDisplay.textContent = '';
+  
+        // Show the "Logout Successful" popup
+        overlay.style.display = 'block';
+        logoutPopup.style.display = 'block';
+      } else {
+        openLoginPopup();
+      }
+    });
+  
+    // Close the "Logout Successful" popup
+    closeLogoutPopupButton.addEventListener('click', function () {
+      overlay.style.display = 'none';
+      logoutPopup.style.display = 'none';
+    });
   });
-});
+  

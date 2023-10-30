83083f73813b02c477c7632cc27fc81fceb7f83c
ifriedham <noah10101@live.com>
2023-10-29 19:13:18 -0600

    starting js for quiz questions

---
+++ quiz.js
@@ -0,0 +1,10 @@
+const quizQuestions = [
+  {
+    question: "Do you like jazz?",
+    answer: ["option1", "option2", "option3", "option4"],
+  },
+  {
+    quesion: "this is a template question",
+    answer: ["option1", "option2", "option3", "option4"],
+  },
+];

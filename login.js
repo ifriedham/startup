ccc0bbdc0ed7f19256fbf476782e3f843c93eed1
ifriedham <noah10101@live.com>
2023-10-29 20:29:37 -0600

    quiz implemented, doesn't work

--- login.js
+++ login.js
@@ -3,72 +3,56 @@ document.addEventListener('DOMContentLoaded', function () {
     const loginButton = document.getElementById('loginButton');
     const closeButton = document.getElementById('closeButton');
     const signupSubmit = document.getElementById('signupSubmit');
-    const logoutButton = document.getElementById('logoutButton'); // New
+    const logoutButton = document.getElementById('logoutButton');
     const usernameInput = document.getElementById('username');
     const loginForm = document.getElementById('loginForm');
-    const successLogoutOverlay = document.getElementById('successLogoutOverlay'); // New
-    const successLogoutCloseButton = document.getElementById('successLogoutCloseButton'); // New
+    const successLogoutOverlay = document.getElementById('successLogoutOverlay');
+    const successLogoutCloseButton = document.getElementById('successLogoutCloseButton');
   
-    // Function to show the popup
     function showPopup() {
       overlay.style.display = 'block';
     }
   
-    // Function to hide the popup
     function hidePopup() {
       overlay.style.display = 'none';
-      usernameInput.value = ''; // Clear the username input
-    }
+      usernameInput.value = '';
   
-    // Function to handle signup
     function handleSignup() {
       const username = usernameInput.value;
       if (username) {
-        // Store the username in local storage
         localStorage.setItem('username', username);
-        // Change the login button to "Logout" and display the username
         loginButton.innerText = 'Logout ' + username;
-        // Hide the popup
         hidePopup();
       } else {
         alert('Please enter a valid username.');
       }
     }
   
-    // Function to handle logout
     function handleLogout() {
-      // Remove the username from local storage
       localStorage.removeItem('username');
-      // Reset the login button to "Login"
       loginButton.innerText = 'Login';
-      // Show the popup
-      //showPopup();
     }
   
-    // Function to show the "successfully logged out" popup
     function showSuccessLogoutPopup() {
       successLogoutOverlay.style.display = 'block';
     }
   
-    // Function to hide the "successfully logged out" popup
     function hideSuccessLogoutPopup() {
       successLogoutOverlay.style.display = 'none';
     }
   
-    // Add event listeners
     loginButton.addEventListener('click', function () {
       if (loginButton.innerText === 'Login') {
         showPopup();
       } else {
         handleLogout();
-        showSuccessLogoutPopup(); // Show "successfully logged out" popup
+        showSuccessLogoutPopup();
       }
     });
   
     closeButton.addEventListener('click', hidePopup);
     signupSubmit.addEventListener('click', handleSignup);
   
-    // Event listener for the "Close" button in the "successfully logged out" popup
     successLogoutCloseButton.addEventListener('click', hideSuccessLogoutPopup);
   
     // Check if a username is stored in local storage
@@ -76,5 +60,5 @@ document.addEventListener('DOMContentLoaded', function () {
     if (storedUsername) {
       loginButton.innerText = 'Logout ' + storedUsername;
     }
-  });
+  }});
   
--- quiz.js
+++ quiz.js
@@ -1,84 +1,108 @@
-
-// proceed to next question + update nav.bar
-document.getElementById('next-button').addEventListener('click', () => {
-    if (questionIndex < quizQuestions.length - 1) {
-      questionIndex++;
-      displayQuestion();
-      progressBar.style.width = `${((questionIndex + 1) / quizQuestions.length) * 100}%`;
-    } else {
-      showResult();
-    }
-  });
-  
-
-//questions
-const quizQuestions = [
-    {
-      question: "Do you like jazz?",
-      answers: ["option1", "option2", "option3", "option4"],
-    },
-    {
-      question: "this is a template question",
-      answers: ["option1", "option2", "option3", "option4"],
-    },
-  ];
-  
-
-//variables
+// Variables
 let questionIndex = 0;
 const userAnswers = [];
+const progressBar = document.querySelector(".progress-bar");
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
 
+// Nav
+prevButton.addEventListener("click", () => { // back
+  if (questionIndex > 0) {
+    questionIndex--;
+    displayQuestion();
+    progressBar.style.width = `${((questionIndex + 1) / quizQuestions.length) * 100}%`;
+  }
+});
+nextButton.addEventListener("click", () => { //next
+  if (questionIndex < quizQuestions.length - 1 && userAnswers[questionIndex] !== undefined) {
+    questionIndex++;
+    displayQuestion();
+    progressBar.style.width = `${((questionIndex + 1) / quizQuestions.length) * 100}%`;
+  }
+});
+
+// Questions
+const quizQuestions = [
+  {
+    question: "Do you like jazz?",
+    answers: ["option1", "option2", "option3", "option4"]
+  },
+  {
+    question: "this is a template question",
+    answers: ["option1", "option2", "option3", "option4"]
+  },
+  {
+    question: "this is another template question",
+    answers: ["option1", "option2", "option3", "option4"]
+  }
+];
 
 function displayQuestion() {
-    const question = quizQuestions[questionIndex];
-    const questionTitle = document.querySelector('.card-title');
-    const choicesContainer = document.getElementById('choices');
-    const progressBar = document.querySelector('.progress-bar');
-  
-    // Update question title
-    questionTitle.textContent = question.question;
-  
-    // Clear previous answer choices
-    choicesContainer.innerHTML = '';
-  
-    // Create answer choice buttons
-    question.answers.forEach((answer, index) => {
-        const answerButton = document.createElement('a');
-      answerButton.href = '#';
-      answerButton.classList.add('btn', 'btn-primary');
-      answerButton.textContent = answer;
-      answerButton.addEventListener('click', () => {
-        userAnswers[questionIndex] = index; // Store the user's answer
-        questionIndex++;
+  const question = quizQuestions[questionIndex];
+  const questionTitle = document.querySelector(".card-title");
+  const choicesContainer = document.getElementById("choices");
 
-        if (questionIndex < quizQuestions.length) {
-          displayQuestion(); // Display the next question
-          progressBar.style.width = `${((questionIndex + 1) / quizQuestions.length) * 100}%`;
-        } 
-        else {
-          showResult(); // Display the recommended album
-        }
-      });
-      choicesContainer.appendChild(answerButton);
+  questionTitle.textContent = question.question;
+  choicesContainer.innerHTML = "";
+
+  question.answers.forEach((answer, index) => {
+    const answerLabel = document.createElement("label");
+    answerLabel.classList.add("btn", "btn-primary");
+
+    const answerInput = document.createElement("input");
+    answerInput.type = "radio";
+    answerInput.name = "answer";
+    answerInput.value = index;
+    answerInput.classList.add("visually-hidden");
+    answerInput.addEventListener("change", () => {
+      userAnswers[questionIndex] = index;
+      questionIndex++;
+
+      // Check if all questions are answered
+      if (questionIndex < quizQuestions.length) {
+        displayQuestion();
+        progressBar.style.width = `${((questionIndex + 1) / quizQuestions.length) * 100}%`;
+      } else {
+        showResult();
+      }
+
+      // Enable "Next" button
+      nextButton.disabled = false;
     });
-  }
-  
-  // Call displayQuestion to start the quiz
-  displayQuestion();
+
+    const answerText = document.createElement("span");
+    answerText.textContent = answer;
+
+    answerLabel.appendChild(answerInput);
+    answerLabel.appendChild(answerText);
+
+    choicesContainer.appendChild(answerLabel);
+  });
+
+  // Disable "Previous" button on the first question
+  prevButton.disabled = questionIndex === 0;
+}
+
+function showResult() {
+  // Calculate recommended
+  const recommendedAlbum = calculateRecommendedAlbum(userAnswers);
+  localStorage.setItem("recommendedAlbum", recommendedAlbum);
+
+  // Display link to results page
+  const questionCard = document.getElementById("quiz-card");
+  questionCard.innerHTML = "";
+
+  const resultMessage = document.createElement("p");
+  resultMessage.textContent = `Recommended Album: ${recommendedAlbum}`;
+
+  questionCard.appendChild(resultMessage);
+}
 
 
-  function showResult() {
-    // Calculate the recommended album based on userAnswers
-    const recommendedAlbum = calculateRecommendedAlbum(userAnswers);
-  
-    // Display the result
-    const questionCard = document.getElementById('quiz-card');
-    questionCard.innerHTML = ''; // Clear the card
-  
-    const resultMessage = document.createElement('p');
-    resultMessage.textContent = `Recommended Album: ${recommendedAlbum}`;
-  
-    questionCard.appendChild(resultMessage);
-  }
-  
-  
+function calculateRecommendedAlbum(userAnswers) {
+  // WIP should return the resulting recommended album
+  return "ERROR: NOT YET IMPLEMENTED";
+}
+
+// Start the quiz
+displayQuestion();

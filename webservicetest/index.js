318a7400f8b041c4ac6597e9732f41139fac882e
ifriedham <noah10101@live.com>
2023-12-10 18:40:30 -0700

    node.js stuff

---
+++ webservicetest/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(`<h1>Hello Node.js! [${req.method}] ${req.url}</h1>`);
+  res.end();
+});
+
+server.listen(8080, () => {
+  console.log(`Web service listening on port 8080`);
+});
